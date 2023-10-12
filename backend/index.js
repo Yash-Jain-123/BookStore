17f38972047cf497922cd591afe4b4a694343b49
Yash <yashjain.12m@gmail.com>
2023-10-12 18:55:29 +0530

    get one book by id

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,7 @@ app.post('/books',async(request,response) => {
     }
 });
 
+//Route for get all books from database
 app.get('/books',async(request,response)=>
 {
     try {
@@ -44,6 +45,19 @@ app.get('/books',async(request,response)=>
             count:books.length,
             data: books
         });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
+
+//Route for get all books from database [by id]
+app.get('/books/:id',async(request,response)=>
+{
+    try {
+        const {id} =request.params;
+        const book= await Book.findById(id);
+        return response.status(200).json(book);
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message});
