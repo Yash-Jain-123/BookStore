4bdf4ddcb57337a5123a76d4c272d673a8d66de8
Yash <yashjain.12m@gmail.com>
2023-10-12 18:10:29 +0530

    get

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-'mongodb+srv://root:root@cluster0.mpvorbj.mongodb.net/books-collection?retryWrites=true&w=majority' 
+'mongodb+srv://root:root@book-store.rruhdzm.mongodb.net/books-collection?retryWrites=true&w=majority' 
